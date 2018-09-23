f87cc2abbb9f83f26d047804ee58a660abf5575d
Peeter Mäeots <maeots.peeter@gmail.com>
2018-09-23 15:46:10 +0300

    new routes created

--- models.js
+++ models.js
@@ -1,18 +1,43 @@
 'use strict'
 
 const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
 
-const postSchema = mongoose.Schema({
-  title: { type: String, required: true },
-  author: {
-    firstName: { type: String, required: true },
-    lastName:  { type: String, required: true }
-  },
-  content: { type: String, required: true}
+const authorSchema = mongoose.Schema({
+  firstName: 'String',
+  lastName:  { type: String, required: true },
+  userName:  { type: String, unique:   true }
 });
 
-postSchema.virtual('authorName').get( function() {
-  return `${this.author.firstName} ${this.author.lastName}`;
+const commentSchema = mongoose.Schema({
+  content: { type: String }
+});
+
+const postSchema = mongoose.Schema({
+  title:   { type: String, required: true },
+  content: { type: String, required: true },
+  author_id:  { type: mongoose.Schema.Types.ObjectId, ref: 'Author' },
+  created: { type: Date },
+  comments: [ commentSchema ]
+});
+
+postSchema.pre('find', function(next) {
+  this.populate('author');
+  next();
+});
+
+postSchema.pre('findByIdAndUpdate', function(next) {
+  this.populate('author');
+  next();
+});
+
+postSchema.pre('findById', function(next) {
+  this.populate('author');
+  next();
+});
+
+postSchema.virtual('authorName').get(function() {
+  return `${this.author.firstName} ${this.author.lastName}`.trim();
 });
 
 postSchema.methods.serialize = function() {
@@ -20,10 +45,13 @@ postSchema.methods.serialize = function() {
     id: this._id,
     title: this.title,
     author: this.authorName,
-    content: this.content
+    created: this.created,
+    content: this.content,
+    comments: this.comments
   }
 }
 
+const Author = mongoose.model('Author', authorSchema);
 const Post = mongoose.model('Post', postSchema);
 
-module.exports = { Post };
+module.exports = { Author, Post };
--- server.js
+++ server.js
@@ -9,44 +9,179 @@ mongoose.Promise = global.Promise;
 
 // get database endpoints
 const { PORT, DATABASE_URL } = require('./config');
-// get posts schema
-const { Post } = require('./models');
+// get schemas
+const { Author, Post } = require('./models');
 
 // set up express
 const app = express();
 app.use(express.json());
+// set up logging
+app.use(morgan('common'));
 
-// GET requests to /posts
-app.get('/posts', (req, res) => {
-  Post.find()
-    // return 10 posts at once max
-    .limit(20)
-    // call .serialize method from models.js to build response
-    .then(posts => {
-      res.json({
-        posts: posts.map( post => post.serialize() )
-      });
+// GET request to /authors
+app.get('/authors', (req, res) => {
+  Author
+    .find()
+    .then(authors => {
+      res.json(authors.map( author => {
+        return {
+          id: author._id,
+          name: `${author.firstName} ${author.lastName}`,
+          userName: author.userName
+        }
+      }));
     })
     .catch(err => {
       console.log(err);
       res.status(500).send({message: 'Internal server error'});
     });
 });
+
+// POST request to /authors
+app.post('/authors', (req, res) => {
+  const requiredFields = ['firstName', 'lastName', 'userName'];
+  for (let i = 0; i < requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if(!(field in req.body)) {
+      const message = `Required field ${field} missing in request body`;
+      console.log(message);
+      return res.status(400).json({message: message});
+    }
+  }
+  Author
+    .findOne({userName: req.body.userName})
+    .then( author => {
+      if (author) {
+        const message = `Entered username is already taken`;
+        console.log(message);
+        return res.status(400).json({message: message});
+      } else {
+        Author.create({
+          firstName: req.body.firstName,
+          lastName: req.body.lastName,
+          userName: req.body.userName
+        })
+        .then( author => {
+          res.status(200).json({
+            _id: author._id,
+            name: `${author.firstName} ${author.lastName}`,
+            userName: author.userName
+          });
+        })
+        .catch( err => {
+          console.log(err);
+          return res.status(500).json({message: 'Error! Unable to add new user!'});
+        });
+      }
+  })
+  .catch( err => {
+    console.log(err);
+    return res.status(500).json({message: 'Unable to connect to server.'});
+  });
+});
+
+// PUT request to /authors
+app.put('/authors/:id', (req, res) => {
+  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+    const message = `Request parameters id ${req.params.id} and request body id ${req.body.id} must match`;
+    console.log(message);
+    return res.status(400).json({ message: message });
+  }
+  const fieldsToUpdate = {};
+  const updateableFields = ['userName', 'firstName', 'lastName'];
+  updateableFields.forEach( field => {
+    if (field in req.body) {
+      fieldsToUpdate[field] = req.body[field];
+    }
+  });
+  Author
+    .findOne({userName: fieldsToUpdate.userName})
+    .then( author => {
+      if (author) {
+        const message = `Entered username is already taken`;
+        console.log(message);
+        return res.status(400).json({message: message});
+      } else {
+        Author
+          .findByIdAndUpdate(req.params.id, {$set: fieldsToUpdate}, {new: true})
+          .then( author => { res.status(200).json({
+            id:       author._id,
+            name:     author.name,
+            userName: author.userName
+          });
+        })
+        .catch( err => {
+          console.log(err);
+          return res.status(500).json({message: 'Internal server error'});
+        });
+      }
+    });
+});
+
+// DELETE request to /authors
+app.delete('/authors/:id', (req, res) => {
+  Post
+    .remove({author: req.params.id})
+    .then( () => {
+      Author
+        .findByIdAndRemove(req.params.id)
+        .then( () => {
+          console.log('Removed user and related posts from database.');
+          res.status(204).json({message: 'User removed.'});
+        })
+        .catch( err => {
+          console.log(err);
+          return res.status(400).json({message: 'Unable to delete user'});
+        });
+    })
+    .catch( err => {
+      console.log(err);
+      return res.status(500).json({message: 'Unable to remove user due to server error'});
+    });
+});
+
+// GET request to /posts
+app.get('/posts', (req, res) => {
+  Post
+    .find()
+    .then(posts => {
+      res.json(posts.map(post => {
+        return {
+          id: post._id,
+          //author: post.authorName,
+          content: post.content,
+          title: post.title
+        };
+      }));
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'Internal server error.' });
+    });
+});
 // GET request to /posts/:id
 app.get('/posts/:id', (req, res) => {
   Post
     .findById(req.params.id)
-    // call .serialize method from models.js to build response
-    .then(post => res.json(post.serialize()))
+    .then(post => {
+      res.json({
+        id: post._id,
+        //author: post.authorName,
+        content: post.content,
+        title: post.title,
+        comments: post.comments
+      });
+    })
     .catch(err => {
-      console.log(err);
-      res.status(500).json({message: 'Internal server error'});
+      console.error(err);
+      res.status(500).json({ error: 'Internal server error.' });
     });
 });
+
 // POST request to /posts
 app.post('/posts', (req, res) => {
   // check for required fields
-  const requiredFields = ['title', 'author', 'content'];
+  const requiredFields = ['title', 'author_id', 'content'];
   for (let i = 0; i < requiredFields.length; i++) {
     const field = requiredFields[i];
     if (!(field in req.body)) {
@@ -55,46 +190,69 @@ app.post('/posts', (req, res) => {
       return res.status(400).json({message: message});
     }
   }
-  // create post 
-  Post.create({
-    title: req.body.title,
-    author: { 
-      firstName: req.body.author.firstName,
-      lastName: req.body.author.lastName
-    },
-    content: req.body.content
-  })
-    .then(post => res.status(201).json(post.serialize()))
+  Author
+    .findById(req.body.author_id)
+    .then( author => {
+      if (author) {
+        Post.create({
+          title: req.body.title,
+          content: req.body.content,
+          author: req.body.author_id
+        })
+        .then( post => 
+          res.status(201).json({
+            id: post.id,
+            title: post.title,
+            content: post.content,
+            author: `${author.firstName} ${author.lastName}`,
+            created: post.created,
+            comments: post.comments
+          }))
+          .catch( err => {
+            console.log(err);
+            return res.status(500).json({message: 'JSON response not sent.'});
+          });
+      } else {
+        const message = 'Unable to post!';
+        console.log(message);
+        return res.status(400).json({message: message});
+      }
+    })
     .catch(err => {
       console.log(err);
-      res.status(500).json({message: 'Internal server error'});
+      return res.status(500).json({message: 'No user found!'});
     });
 });
 // PUT request to /posts
 app.put('/posts/:id', (req, res) => {
   // compare req path and req body ids
-  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
+  if (!(req.params.id && req.body._id && req.params.id === req.body._id)) {
     const message = `Request path id (${req.params.id}) and request body id (${req.body.id}) have to match`;
     console.log(message);
     return res.status(400).json({message: message});
   }
   // look for matching fields in request body
-  const toUpdate = {};
-  const updateableFields = [
-    'title', 
-    'content', 
-    'author'
-  ];
-  updateableFields.forEach(field => {
+  const fieldToUpdate = {};
+  const updateableFields = [ 'title', 'content' ];
+  updateableFields.forEach( field => {
     if (field in req.body) {
-      toUpdate[field] = req.body[field];
+      fieldToUpdate[field] = req.body[field];
     }
   });
   // update fields found 
   Post
-    .findByIdAndUpdate(req.params.id, {$set: toUpdate})
-    .then(post => res.status(201).end())
-    .catch(err => res.status(500).json({message: 'Internal server error'}));
+    .findByIdAndUpdate(req.params.id, {$set: fieldToUpdate}, {new: true})
+    .then( post => res.status(200).json({
+      id: post.id,
+      title: post.title,
+      content: post.content,
+      author: post.author_id,
+      created: Date.now().toString()
+    }))
+    .catch( err => {
+      console.log(err);
+      return res.status(500).json({message: 'Unable to update post due to server error.'});
+    });
 });
 // DELETE request to /posts
 app.delete('/posts/:id', (req, res) => {
